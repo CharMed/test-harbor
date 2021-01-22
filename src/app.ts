ac048b81b31458c9e15d73167196e359d42691e3
maryna.usatiuk <maryna.usatiuk@tmdhosting.com>
2021-01-22 13:54:03 +0200

    chore: lint

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'pixi.js';
 
 class Game {
   private app: Application;
+
   private width = window.innerWidth - 100;
+
   private height = window.innerHeight - 100;
+
   constructor() {
     // instantiate app
     this.app = new Application({
       width: this.width,
+
       height: this.height,
+
       backgroundColor: 0x1099bb,
     });
 
     // create view in DOM
     document.body.appendChild(this.app.view);
-
-  
   }
 
+  // eslint-disable-next-line class-methods-use-this
   setup(): void {
   }
 }
