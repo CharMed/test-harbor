24a73f920e20383ac6e129763286d14973aa9861
maryna.usatiuk <maryna.usatiuk@tmdhosting.com>
2021-01-22 12:24:16 +0200

    chore: use index template by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'TestHarbor',
+      template: 'src/index.html',
     }),
     new CopyPlugin({
       patterns: [{
