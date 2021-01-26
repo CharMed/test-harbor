9a27e611c6cebe67e687a05c5a5946fbaee8bc55
maryna.usatiuk <maryna.usatiuk@tmdhosting.com>
2021-01-26 15:15:23 +0200

    feat: resize main based to window width

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,32 @@ import { Application } from 'pixi.js';
 
 import { Sea } from './sea';
 import './development';
+import { SIZE } from './utils/constant';
 
 class Game {
   private app: Application;
 
   #sea: Sea | undefined;
 
+  #scale = 1;
+
   constructor() {
+    const width = (window.innerWidth - 100) / 2;
+    const height = (width * 9) / 16;
+    const view = document.getElementById('view') as HTMLCanvasElement;
+
     // instantiate app
     this.app = new Application({
-      width: 480,
-      height: 270,
+      view,
+      width,
+      height,
       backgroundColor: 0x1099bb,
     });
-
+    this.#scale = width / SIZE.SEA.width;
     // create view in DOM
-    document.body.appendChild(this.app.view);
+    if (!view) {
+      document.body.appendChild(this.app.view);
+    }
     this.setup();
   }
 
@@ -30,6 +40,7 @@ class Game {
   setup(): void {
     this.#sea = new Sea();
     this.app.stage.addChild(this.#sea.getView());
+    this.app.stage.scale.set(this.#scale, this.#scale);
     this.app.ticker.add(this.onUpdateTicker.bind(this));
   }
 }
